babcc54b662c906cc517d11d11e55245be2b733a
bgates747 <brandon.gates@gmail.com>
2026-07-27 22:31:26 -0400

    Correct Pingo texture row orientation

--- video/pingo/render/texture.c
+++ video/pingo/render/texture.c
@@ -27,13 +27,15 @@ Pixel texture_read(Texture *f, Vec2i pos)
 
 Pixel texture_readF(Texture *f, Vec2f pos)
 {
-    uint16_t x = (uint16_t)(pos.x * f->size.x) % f->size.x;
-    uint16_t y = (uint16_t)(pos.y * f->size.y) % f->size.x;
+    float u = fminf(fmaxf(pos.x, 0.0f), 1.0f);
+    float v = fminf(fmaxf(pos.y, 0.0f), 1.0f);
+    uint16_t x = (uint16_t)(u * (f->size.x - 1));
+    // UV V grows upward; texture memory begins with the top image row.
+    uint16_t y = (uint16_t)((1.0f - v) * (f->size.y - 1));
     uint32_t index = x + y * f->size.x;
     Pixel value = f->frameBuffer[index];
     return value;
 }
-
 
 
 
